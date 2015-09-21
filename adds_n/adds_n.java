f9004ca7953c3aae4bc98e3ae4dfa36633bf5477
iantheninja <munene.ian@live.com>
2015-09-21 10:33:20 +0300

    student demo

--- adds_n/adds_n.java
+++ adds_n/adds_n.java
@@ -1,25 +1,26 @@
 //Using Java, have the function adds_n(num) add up all the numbers from 1 to num.
-// num is a number entered in by a user(you) when prompted from the terminal 
-//For the test cases, the parameter num will be any number from 1 to 1000. 
+// num is a number entered in by a user(you) when prompted from the terminal
+//For the test cases, the parameter num will be any number from 1 to 1000.
 
 import java.util.Scanner;
 public class User {
-	
-	static int adds_n(int num) { 
+
+	int adds_n(int num) {
 		if(num > 1000){
 			System.out.println("unknown number");
 		}else{
 	       for(int i = 0; i <= num; i++){
-	    	   System.out.println(i);	
+	    	   System.out.println(i);
 	       }
 		}
 	    return num;
-	    
-	  } 
+
+	  }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner  s = new Scanner(System.in);
-	    	System.out.print(adds_n(s.nextInt()));
+    User usr = new User();
+	  System.out.print(usr.adds_n(s.nextInt()));
 	}
 
 }
