32b8032bec2624aac534ab3d5258f91ccb6e0a92
Moses Okemwa <okemwamoses@gmail.com>
2015-09-21 11:56:16 +0300

    firstnty at the solution

--- capitalize/capitalize.java
+++ capitalize/capitalize.java
@@ -3,4 +3,22 @@
 // the scanner object should be in the main method, from where we are going to call our function for testing
 // it should take string input from a user
 
+public class capitalize {
+
+	/**
+	 * @author Okemwa
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		words= string.split(" ");
+		string = " ";
+		for(String w: words)
+			word = word.toUpperCase().replace(word.substring(1)), word.substring(1).toLowerCase());
+			string += word;
+
+
+	}
+
+}
+
 
