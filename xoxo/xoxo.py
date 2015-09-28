46f4351df1396be6b09a2644f73d30da44b5ac8e
Moses Okemwa <okemwamoses@gmail.com>
2015-09-28 09:55:55 +0300

    letter count using python

--- xoxo/xoxo.py
+++ xoxo/xoxo.py
@@ -5,8 +5,30 @@
 # no punctuation or numbers.
 # For example: if str is "xooxxxxooxo" then the output should return false because there are 6 x's and 5 o's.
 
-def xoxo(str):
-  # code goes here
+def Xoxo(str):
+
+#	xcount = 0
+#	ocount = 0
+#
+#	for letter in str:
+#		for v in letter:
+#			if v == 'x':
+#				xcount += len(v)
+#			elif v == 'o':
+#				ocount += len(v)
+#	if xcount == ocount:
+#		return True
+#	else:
+#		return False
+#
+#
+#				or
+
+
+	if str.count('x') == str.count('o'):
+		return True
+	return False
+
 
 
 # keep the function call
