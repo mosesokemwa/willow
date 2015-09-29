42d952a265f012fe5cb6dad6c19fc5791fa08b20
Moses Okemwa <okemwamoses@gmail.com>
2015-09-29 13:16:11 +0300

    use of ascii key in python instead of caseswap method

--- swapie/swapie.py
+++ swapie/swapie.py
@@ -4,3 +4,29 @@
 #Let numbers and symbols stay the way they are.
 
 #NB do not use the swapcase python method.
+import string;
+
+def SwapCase(str):
+
+	new_string = '';
+	alphabet_length = len(string.ascii_lowercase);
+
+	for character in str:
+		if (character in string.ascii_lowercase):
+			for p in range (alphabet_length):
+				if (string.ascii_lowercase[p] == character):
+					new_string += string.ascii_uppercase[p];
+					break;
+
+		else:
+			if(character in string.ascii_uppercase):
+				for p in range (alphabet_length):
+					if (string.ascii_uppercase[p] == character):
+						new_string += string.ascii_lowercase[p];
+						break;
+
+			else:
+				new_string += character;
+	return new_string
+
+print SwapCase("The Broom Is TOO LONG FoR no reAson")
