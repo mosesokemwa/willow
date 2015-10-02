f69365ba09a21e81db784fa0ee755a0ec0fd4f06
Moses Okemwa <okemwamoses@gmail.com>
2015-10-02 10:59:19 +0300

    solution

--- swapie/swapie.py
+++ swapie/swapie.py
@@ -9,25 +9,40 @@ import string;
 
 def SwapCase(str):
 
+	#initialize an empty list
 	new_string = '';
+
+	#asigning
 	alphabet_length = len(string.ascii_lowercase);
 
+	#looping through string
 	for character in str:
+
+		#checking whether string contains lower case
 		if (character in string.ascii_lowercase):
 			for p in range (alphabet_length):
+
+				#turning lowercase letters to uppercase
+				#adds new uppercase letters to the list new_string
 				if (string.ascii_lowercase[p] == character):
 					new_string += string.ascii_uppercase[p];
 					break;
 
 		else:
+			#checking whether string contains upper case letters
 			if(character in string.ascii_uppercase):
 				for p in range (alphabet_length):
+
+					#turning uppercase letters to lowercase
+					#adds new lowercase letters to the list new_string
 					if (string.ascii_uppercase[p] == character):
 						new_string += string.ascii_lowercase[p];
 						break;
 
 			else:
+				#this should account for the non letter items
 				new_string += character;
 	return new_string
 
-print SwapCase("The Broom Is TOO LONG FoR no reAson")
+print SwapCase("It DOeS nothing to INTEGRs like 662258793, @Booleans & WierD ChARCterS like ??:'{}[[=--+++-+-+-++++-+-+.")
+
