cb4fa3260d18220b47f38bdfa32e6443dc5f7327
Moses Okemwa <okemwamoses@gmail.com>
2015-10-02 09:30:01 +0300

    solution

--- gigasecond/gigasecond.py
+++ gigasecond/gigasecond.py
@@ -26,8 +26,18 @@ from datetime import date,timedelta
 WEEKDAYS = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
 
 def get_user_b_date(year,month,day):
+
+	#getting to be lived before giga_second
 	giga_birthsecond = date(year, month, day) + timedelta(seconds=10 ** 9)
-	birthday = WEEKDAYS[giga_birthsecond.weekday()]
+
+	#calling on import datetime using WEEKDAYS list
+	anniversary = WEEKDAYS[giga_birthsecond.weekday()]
+
+	#time to be lived minus current date(which varies)
 	days_left = (giga_birthsecond - date.today()).days
-	print(giga_birthsecond, birthday, days_left, "days left")
-print(get_user_b_date(1990,1,24))
+
+	#output
+	print(anniversary, days_left, "days left")
+
+#calling our function with a random variable
+print(get_user_b_date(1991,1,24))
