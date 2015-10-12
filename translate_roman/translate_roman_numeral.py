8d7811efeb97a2ec0af9b997ac3f68ea194e054c
Moses Okemwa <okemwamoses@gmail.com>
2015-10-12 09:25:39 +0300

    solution

--- translate_roman/translate_roman_numeral.py
+++ translate_roman/translate_roman_numeral.py
@@ -15,11 +15,20 @@
 
 
 def romanNumeral(string):
+	# this accounts for lower and upper case roman numeral input in a seperate list
 	values = {"i":1, "v":5, "x":10, "l":50, "c":100, "m":1000}
+	values = {"I":1, "V":5, "X":10, "L":50, "C":100, "M":1000} 
+
+	# better way of doing it
+
+	values = {
+				"i":1, "v":5, "x":10, "l":50, "c":100, "m":1000,
+				"I":1, "V":5, "X":10, "L":50, "C":100, "M":1000
+			}
 
 
 	return sum(map(lambda x: values[x], string))
-print romanNumeral("vxiilm")
+print romanNumeral("iV")
 
 
 #map(func, seq) is a function with two arguments
