cff0647573a53342c91a43a2da7ddb4b96cc6ade
Moses Okemwa <okemwamoses@gmail.com>
2015-10-19 09:42:54 +0300

    solution

--- GCF/GCF.java
+++ GCF/GCF.java
@@ -3,3 +3,41 @@
 // That is, return the greatest number that evenly goes into both numbers with no remainder.
 // For example: 20 and 10 both are divisible by 1, 2, 5 and 10 so the output should be 10.
 // The range for both parameters will be from 1 to 10^3.
+
+// public class GCF {
+//
+//
+//     public static void main(String [] args){
+//
+//       System.out.print(divide(54, 24));
+//     }
+//
+//     private static int divide(int num1, int num2) {
+//         //base case
+//         if(num2 == 0){
+//             return num1;
+//         }
+//         return divide(num2, num1%num2);
+//     }
+// }
+
+
+public class GCF {
+    public static void main(String [] args){
+
+      System.out.print(divide(54, 24));
+    }
+
+    private static int divide(int num1, int num2) {
+      //base case
+      gcd =  1;
+
+      for (int i = 0; i < (10^3); i++){
+        if(num2%i == 0 && num1%i == 0){
+          return i;
+        gcd += i;
+        }
+      // return divide(num2, num1%num2);
+      }
+  }
+}
