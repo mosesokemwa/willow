a22bcdf04d8624d0883953a4a4fb7986deec1e18
Moses Okemwa <okemwamoses@gmail.com>
2015-10-23 10:38:00 +0300

    removes duplicates in a string

--- remove_duplicate/remove_duplicate.py
+++ remove_duplicate/remove_duplicate.py
@@ -11,3 +11,25 @@ def removeDuplicates(string):
 print removeDuplicates("tree transversal")
 print removeDuplicates("characters")
 print removeDuplicates("5521866958")
+
+
+
+# set method cannot contain duplicates
+
+def removeDuplicates(seq):
+    seen = set()
+    seen_add = seen.add
+    return [ x for x in seq if not (x in seen or seen_add(x))]
+
+print removeDuplicates("tree transversal")
+print removeDuplicates("characters")
+print removeDuplicates("5521866958")
+
+
+
+def removeDuplicate(alist):
+    return (list(set(alist))
+
+print removeDuplicates("tree transversal")
+print removeDuplicates("characters")
+print removeDuplicates("5521866958")
