bdc962526047d1418a3b2a01e18c2166b708f331
Moses Okemwa <okemwamoses@gmail.com>
2015-10-27 09:38:12 +0300

    reverse word program

--- reverse_words/reverse_words.java
+++ reverse_words/reverse_words.java
@@ -3,3 +3,18 @@
 // Words are always delimited by spaces.
 // For example if the given word is "reverse words of a sentence".
 // The output will be "sentence a of words reverse"
+
+class Main {
+
+  public static void ReverseString(String str){
+
+    String [] str_arr = str.split(" ");
+    for(int i = str_arr.length -1; i >= 0; i--) {
+        System.out.print(str_arr[i]+" ");
+    }
+
+  }
+  public static void main(String[] args) {
+    ReverseString("hello world");
+  }
+}
